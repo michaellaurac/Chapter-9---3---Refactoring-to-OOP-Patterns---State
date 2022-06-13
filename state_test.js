94b811c53ade0199eaa615b39374b58625dac2e8
Michaël L <michaellaurac@gmail.com>
2022-06-13 11:32:09 +0200

    Added a change method to the Person class and its respective forget and learn methods in the subclasses in order to complete the state pattern. Relevant testing was added as well. Note that wish was replaced by assert since wish did not allow both states to be tested.

--- state_test.js
+++ state_test.js
@@ -5,34 +5,58 @@ const fileName = () => {
 
 /* eslint-env mocha */
 
-const wish = require("wish");
+const assert = require("assert");
 const { Person, binaryAwareness, binaryObliviousness } = require("./state.js");
 
 describe("tests run on the 'state.js' file:", () => {
   // setup test
   it("verifies the test file name", () => {
-    wish(fileName() === "state_test.js");
+    assert(fileName(), "state_test.js");
   });
   // initialisation
   const personOne = new Person(binaryAwareness);
   const personTwo = new Person(binaryObliviousness);
   // functional tests
-  it("checks that '10' is read as '2' for a binary read aware", () => {
-    wish(personOne.binaryKnowledge.read(10) === 2);
+  it("checks that '10' is read as '2' for a binary read aware such as personOne", () => {
+    assert(personOne.binaryKnowledge.read(10), 2);
   });
-  it("checks that '10' is read as '10' for a binary read oblivious", () => {
-    wish(personTwo.binaryKnowledge.read(10) === 10);
+  it("checks that '10' is read as '10' for a binary read oblivious such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.read(10), 10);
   });
-  it("checks that '2 & 3' yields '2' for a binary and knower", () => {
-    wish(personOne.binaryKnowledge.and(2, 3) === 2);
+  it("checks that '2 & 3' yields '2' for a binary and knower such as personOne", () => {
+    assert(personOne.binaryKnowledge.and(2, 3), 2);
   });
-  it("checks that '2 & 3' yields 'unknown' for a binary and oblivious", () => {
-    wish(personTwo.binaryKnowledge.and(2, 3) === "unknown");
+  it("checks that '2 & 3' yields 'unknown' for a binary and oblivious such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.and(2, 3), "unknown");
   });
-  it("checks that '2 ^ 3' yields '1' for a binary xor knower", () => {
-    wish(personOne.binaryKnowledge.xor(2, 3) === 1);
+  it("checks that '2 ^ 3' yields '1' for a binary xor knower such as personOne", () => {
+    assert(personOne.binaryKnowledge.xor(2, 3), 1);
   });
-  it("checks that '2 ^ 3' yields 'unknown' for a binary xor oblivious", () => {
-    wish(personTwo.binaryKnowledge.xor(2, 3) === "unknown");
+  it("checks that '2 ^ 3' yields 'unknown' for a binary xor oblivious such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.xor(2, 3), "unknown");
+  });
+  // state change
+  personOne.binaryKnowledge.forget();
+  personTwo.binaryKnowledge.learn();
+
+  console.log(personOne.binaryKnowledge.read(10));
+  // functional tests
+  it("checks that '10' is read as '2' for a binary read aware such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.read(10), 2);
+  });
+  it("checks that '10' is read as '10' for a binary read oblivious such as personOne", () => {
+    assert(personOne.binaryKnowledge.read(10), 10);
+  });
+  it("checks that '2 & 3' yields '2' for a binary and knower such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.and(2, 3), 2);
+  });
+  it("checks that '2 & 3' yields 'unknown' for a binary and oblivious such as personOne", () => {
+    assert(personOne.binaryKnowledge.and(2, 3), "unknown");
+  });
+  it("checks that '2 ^ 3' yields '1' for a binary xor knower such as personTwo", () => {
+    assert(personTwo.binaryKnowledge.xor(2, 3), 1);
+  });
+  it("checks that '2 ^ 3' yields 'unknown' for a binary xor oblivious such as personOne", () => {
+    assert(personOne.binaryKnowledge.xor(2, 3), "unknown");
   });
 });
